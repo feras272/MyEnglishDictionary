afb20820db73d8f50e6d2a3cd537d07f4b86d9bf
feras alnowiser <ferasdev@gmail.com>
2022-10-31 04:03:56 +0300

    add: static WordRoomDb instance, abstract WordsDao function, and WordRoomDb singleton function to ensure creating the database once and use the same database was created

--- app/src/main/java/com/example/myenglishdictionary/WordRoomDb.java
+++ app/src/main/java/com/example/myenglishdictionary/WordRoomDb.java
@@ -1,26 +1,50 @@
 package com.example.myenglishdictionary;
 
-import androidx.annotation.NonNull;
-import androidx.room.DatabaseConfiguration;
-import androidx.room.InvalidationTracker;
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
-import androidx.sqlite.db.SupportSQLiteOpenHelper;
 
-public class WordRoomDb extends RoomDatabase {
-    @NonNull
-    @Override
-    protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration config) {
-        return null;
+/*
+@Database(entities = {student.class, version = 1}, {organization.class, version = 1})
+ */
+
+@Database(entities = Words.class, version = 1)
+public abstract class WordRoomDb extends RoomDatabase {
+
+    // we created this class to transform this class into Singleton
+    // We can't create copy or more than a copy/instance from the same database
+    private static WordRoomDb instance;
+
+    // for accessing the Room operations (CRUD)
+    public abstract WordsDao wordDao();
+
+    // Singleton - Only one thread can access to this function and interact with the database
+    public static synchronized WordRoomDb getInstance(Context context) {
+        if(instance == null) {
+            // return to me for a copy from database
+            instance = Room.databaseBuilder(context.getApplicationContext(), WordRoomDb.class, "word-database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
     }
 
-    @NonNull
-    @Override
-    protected InvalidationTracker createInvalidationTracker() {
-        return null;
-    }
-
-    @Override
-    public void clearAllTables() {
-
-    }
+//    @NonNull
+//    @Override
+//    protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration config) {
+//        return null;
+//    }
+//
+//    @NonNull
+//    @Override
+//    protected InvalidationTracker createInvalidationTracker() {
+//        return null;
+//    }
+//
+//    @Override
+//    public void clearAllTables() {
+//
+//    }
 }
