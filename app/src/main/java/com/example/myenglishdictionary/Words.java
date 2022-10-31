454a1a3fbd14e19345f7e4bc68e85bd708c252c1
feras alnowiser <ferasdev@gmail.com>
2022-10-31 03:15:39 +0300

    add: Words variables, constructors, setters, and getters encapsulate the Words class into Entity object generate auto id by using PrimaryKey annotation

--- app/src/main/java/com/example/myenglishdictionary/Words.java
+++ app/src/main/java/com/example/myenglishdictionary/Words.java
@@ -1,4 +1,42 @@
 package com.example.myenglishdictionary;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "wordTable")
 public class Words {
+    // primary key
+    @PrimaryKey(autoGenerate = true) // use the autoGenerate feature
+    private int id;
+    private String wordName;
+    private String wordMeaning;
+    @ColumnInfo(name = "Type")
+    private String wordType;
+
+    public Words(String wordName, String wordMeaning, String wordType) {
+        this.wordName = wordName;
+        this.wordMeaning = wordMeaning;
+        this.wordType = wordType;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getWordName() {
+        return wordName;
+    }
+
+    public String getWordMeaning() {
+        return wordMeaning;
+    }
+
+    public String getWordType() {
+        return wordType;
+    }
 }
