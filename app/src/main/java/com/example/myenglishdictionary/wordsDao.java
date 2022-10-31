ddd700971a09cc7bf757b774400b32e0ed22e9ea
feras alnowiser <ferasdev@gmail.com>
2022-10-31 03:36:29 +0300

    add: all the functions needed for CRUD operations

--- app/src/main/java/com/example/myenglishdictionary/wordsDao.java
+++ app/src/main/java/com/example/myenglishdictionary/wordsDao.java
@@ -1,4 +1,31 @@
 package com.example.myenglishdictionary;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
 public interface wordsDao {
+
+    @Insert
+    void insert(Words word);
+
+    @Update
+    void update(Words word);
+
+    @Delete
+    void delete(Words word);
+
+    @Query("DELETE FROM wordTable")
+    void deleteAllWords();
+
+    @Query("SELECT * FROM wordTable")
+    LiveData<List<Words>> getAllWords(); // LiveData will notify the interface for the changes, Monitor the list continuously
+
+
 }
